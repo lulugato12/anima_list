a680279b5ad311b5c52aec16214923aeab132923
Lourdes Cajica <A01732177@itesm.mx>
2022-06-20 03:16:11 -0500

    closes #15; Controller - listas, add Many2Many for Genre

--- backend/animalist/animes/models.py
+++ backend/animalist/animes/models.py
@@ -9,7 +9,7 @@ class Genre(models.Model):
 
 class Anime(models.Model):
     title = models.CharField(max_length = 50)
-    genre = models.ForeignKey('Genre', on_delete=models.RESTRICT, related_name = 'animes')
+    genres = models.ManyToManyField('Genre', related_name = 'animes')
     tipo = models.ForeignKey('Tipo', on_delete=models.RESTRICT, related_name = 'animes')
     episodes = models.IntegerField(null = True)
     synopsis = models.TextField()
@@ -19,3 +19,8 @@ class Rating(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.SET_NULL, related_name = 'ratings', null = True)
     anime = models.ForeignKey('Anime', on_delete = models.CASCADE, related_name = 'ratings')
     value = models.IntegerField()
+
+class Lista(models.Model):
+    name = models.CharField(max_length = 30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = 'listas')
+    animes = models.ManyToManyField('Anime', related_name = 'listas')
--- backend/animalist/animes/schema.py
+++ backend/animalist/animes/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Anime, Tipo, Genre, Rating
+from .models import Anime, Tipo, Genre, Rating, Lista
 
 class TipoType(DjangoObjectType):
     class Meta:
@@ -17,6 +17,10 @@ class AnimeType(DjangoObjectType):
 class RatingType(DjangoObjectType):
     class Meta:
         model = Rating
+
+class ListaType(DjangoObjectType):
+    class Meta:
+        model = Lista
 
 class CreateTipo(graphene.Mutation):
     tipo = graphene.Field(TipoType)
@@ -85,17 +89,77 @@ class CreateRating(graphene.Mutation):
 
         return CreateRating(rating = rating)
 
+class CreateLista(graphene.Mutation):
+    lista = graphene.Field(ListaType)
+
+    class Arguments:
+        name = graphene.String(required = True)
+
+    def mutate(self, info, name):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('log in >:(.')
+
+        lista = Lista(user = user, name = name)
+        lista.save()
+
+        return CreateLista(lista = lista)
+
+class AddAnime(graphene.Mutation):
+    lista = graphene.Field(ListaType)
+
+    class Arguments:
+        anime_id = graphene.Int(required = True)
+        lista_id = graphene.Int(required = True)
+
+    def mutate(self, info, anime_id, lista_id):
+        anime = Anime.objects.filter(id = anime_id).first()
+        if not anime:
+            raise Exception('invalid anime.')
+
+        lista = Lista.objects.filter(id = lista_id).first()
+        if not lista:
+            raise Exception('invalid lista.')
+
+        lista.animes.add(anime)
+
+        return AddAnime(lista = lista)
+
+class AddGenre(graphene.Mutation):
+    anime = graphene.Field(AnimeType)
+
+    class Arguments:
+        anime_id = graphene.Int(required = True)
+        genre_id = graphene.Int(required = True)
+
+    def mutate(self, info, anime_id, genre_id):
+        anime = Anime.objects.filter(id = anime_id).first()
+        if not anime:
+            raise Exception('invalid anime.')
+
+        genre = Genre.objects.filter(id = genre_id).first()
+        if not genre:
+            raise Exception('invalid genre.')
+
+        anime.genres.add(genre)
+
+        return AddGenre(anime = anime)
+
 class Mutation(graphene.ObjectType):
     create_tipo = CreateTipo.Field()
     create_genre = CreateGenre.Field()
     create_anime = CreateAnime.Field()
     create_rating = CreateRating.Field()
+    create_lista = CreateLista.Field()
+    add_anime = AddAnime.Field()
+    add_genre = AddGenre.Field()
 
 class Query(graphene.ObjectType):
     animes = graphene.List(AnimeType)
     tipos = graphene.List(TipoType)
     genres = graphene.List(GenreType)
     ratings = graphene.List(RatingType)
+    listas = graphene.List(ListaType)
 
     def resolve_animes(self, info):
         return Anime.objects.all()
@@ -108,3 +172,6 @@ class Query(graphene.ObjectType):
 
     def resolve_ratings(self, info):
         return Rating.objects.all()
+
+    def resolve_listas(self, info):
+        return Lista.objects.all()
