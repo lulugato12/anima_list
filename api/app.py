a36f5d250523f6dc9543c68a072bf2284c65fde8
Lourdes Cajica <A01732177@itesm.mx>
2022-06-21 12:17:22 -0500

    create recommendation api

---
+++ api/app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+import numpy as np
+import pandas as pd
+import itertools
+import collections
+from sklearn.metrics import jaccard_score
+import json
+
+app = FastAPI()
+
+# Preprocessing
+animes = pd.read_csv('AnimeList.csv')
+animes['genre'] = animes['genre'].fillna('None')
+animes['genre'] = animes['genre'].apply(lambda x: x.split(', '))
+
+genre_data = itertools.chain(*animes['genre'].values.tolist())
+genre_counter = collections.Counter(genre_data)
+genres = pd.DataFrame.from_dict(genre_counter, orient='index').reset_index().rename(columns={'index':'genre', 0:'count'})
+genres.sort_values('count', ascending=False, inplace=True)
+
+genre_map = {genre: idx for idx, genre in enumerate(genre_counter.keys())}
+def extract_feature(genre):
+    feature = np.zeros(len(genre_map.keys()), dtype=int)
+    feature[[genre_map[idx] for idx in genre]] += 1
+    return feature
+
+anime_feature = pd.concat([animes['title'], animes['genre']], axis=1)
+anime_feature['genre'] = anime_feature['genre'].apply(lambda x: extract_feature(x))
+
+@app.get("/{id}")
+def read_root(id):
+    test_data = anime_feature.take([id])
+    output = None
+    for row in test_data.iterrows():
+        search = anime_feature.drop([row[0]])
+        search['result'] =  search['genre'].apply(lambda x: jaccard_score(row[1]['genre'], x))
+        search_result = search.sort_values('result', ascending=False)['title'].head(10)
+        indexes = search_result.index
+        output = json.dumps({"animes": list(zip(indexes, search_result.to_dict().values(), animes['image_url'][indexes].to_dict().values()))})
+
+    return output
