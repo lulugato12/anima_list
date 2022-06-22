1ba9504bb83f588a1c3a9e389d2f6d98c34d3514
Lourdes Cajica <A01732177@itesm.mx>
2022-06-21 21:45:35 -0500

    remove duplicated graphql

--- backend/animalist/animalist/urls.py
+++ backend/animalist/animalist/urls.py
@@ -22,6 +22,5 @@ from animalist.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("graphql/",jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True,schema=schema))))
 ]
